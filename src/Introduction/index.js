b4e5421700ee8336dbc11616115efd07112743b5
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-06-21 17:28:40 +0300

    pushed my react work

---
+++ src/Introduction/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css'
+
+const Introduction = ()=>{
+    return(
+        <div>
+            <h1>What I learn in React</h1>
+            <p>It is a Js library developed by facebook</p>
+            <p>It has a reusable component</p>
+        </div>
+    )
+}
+export default Introduction;
